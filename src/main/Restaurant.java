db50efd09ed6061d001038f8f059f05b358e8f6d
VelvetHawk <artem.smnv@yahoo.ie>
2019-03-10 14:35:36 +0000

    Added gradle build support, reworked some paths

--- src/main/Restaurant.java
+++ src/main/Restaurant.java
@@ -14,20 +14,22 @@ public class Restaurant extends Application
     public void start(Stage primaryStage)
     {
         ScreensController mainContainer = new ScreensController();
+    
+        final String fxmlRootPath = "../../resources/fxml/";
         
         // Screens
-        mainContainer.loadScreen(Screens.MAIN, "/display/views/mainScreen.fxml");
-        mainContainer.loadScreen(Screens.LOGIN, "/display/views/login.fxml");
-        mainContainer.loadScreen(Screens.REGISTRATION, "/display/views/registration.fxml");
-        mainContainer.loadScreen(Screens.MAIN_MENU, "/display/views/mainMenu.fxml");
-        mainContainer.loadScreen(Screens.MAKE_ORDER, "/display/views/makeOrder.fxml");
+        mainContainer.loadScreen(Screens.MAIN, fxmlRootPath + "mainScreen.fxml");
+        mainContainer.loadScreen(Screens.LOGIN, fxmlRootPath + "login.fxml");
+        mainContainer.loadScreen(Screens.REGISTRATION, fxmlRootPath + "registration.fxml");
+        mainContainer.loadScreen(Screens.MAIN_MENU, fxmlRootPath + "mainMenu.fxml");
+        mainContainer.loadScreen(Screens.MAKE_ORDER, fxmlRootPath + "makeOrder.fxml");
 		
         // Pop Up Screens
-	    mainContainer.loadPopUpScreen(PopUpScreens.ORDER_TYPE_CHOICE, "/display/components/OrderTypeChoice.fxml");
-	    mainContainer.loadPopUpScreen(PopUpScreens.SELECT_DRINK, "/display/views/drinks.fxml");
-	    mainContainer.loadPopUpScreen(PopUpScreens.SELECT_FOOD, "/display/views/food.fxml");
-        mainContainer.loadPopUpScreen(PopUpScreens.SELECT_TOPPING, "/display/views/toppings.fxml");
-        mainContainer.loadPopUpScreen(PopUpScreens.SELECT_SIDE, "/display/views/sides.fxml");
+	    mainContainer.loadPopUpScreen(PopUpScreens.ORDER_TYPE_CHOICE, fxmlRootPath + "OrderTypeChoice.fxml");
+	    mainContainer.loadPopUpScreen(PopUpScreens.SELECT_DRINK, fxmlRootPath + "drinks.fxml");
+	    mainContainer.loadPopUpScreen(PopUpScreens.SELECT_FOOD, fxmlRootPath + "food.fxml");
+        mainContainer.loadPopUpScreen(PopUpScreens.SELECT_TOPPING, fxmlRootPath + "toppings.fxml");
+        mainContainer.loadPopUpScreen(PopUpScreens.SELECT_SIDE, fxmlRootPath + "sides.fxml");
         
         mainContainer.setScreen(Screens.MAIN); // set the main screen at the start
         // grouping the scene to root.
