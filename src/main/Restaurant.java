da68375d13e6200967540f90fbcb41bdd1c201a3
Conor Moloney <conormoloney57@gmail.com>
2019-04-02 13:29:05 +0000

    restaurant

--- src/main/Restaurant.java
+++ src/main/Restaurant.java
@@ -14,23 +14,24 @@ public class Restaurant extends Application
     public void start(Stage primaryStage)
     {
         ScreensController mainContainer = new ScreensController();
-    
-        final String fxmlRootPath = "../../resources/fxml/";
-        
+
+//        final String fxmlRootPath = "../../resources/fxml/";
+        final String fxmlRootPath = "/resources/fxml/";
+
         // Screens
         mainContainer.loadScreen(Screens.MAIN, fxmlRootPath + "mainScreen.fxml");
         mainContainer.loadScreen(Screens.LOGIN, fxmlRootPath + "login.fxml");
         mainContainer.loadScreen(Screens.REGISTRATION, fxmlRootPath + "registration.fxml");
         mainContainer.loadScreen(Screens.MAIN_MENU, fxmlRootPath + "mainMenu.fxml");
         mainContainer.loadScreen(Screens.MAKE_ORDER, fxmlRootPath + "makeOrder.fxml");
-		
+
         // Pop Up Screens
-	    mainContainer.loadPopUpScreen(PopUpScreens.ORDER_TYPE_CHOICE, fxmlRootPath + "OrderTypeChoice.fxml");
-	    mainContainer.loadPopUpScreen(PopUpScreens.SELECT_DRINK, fxmlRootPath + "drinks.fxml");
-	    mainContainer.loadPopUpScreen(PopUpScreens.SELECT_FOOD, fxmlRootPath + "food.fxml");
+        mainContainer.loadPopUpScreen(PopUpScreens.ORDER_TYPE_CHOICE, fxmlRootPath + "OrderTypeChoice.fxml");
+        mainContainer.loadPopUpScreen(PopUpScreens.SELECT_DRINK, fxmlRootPath + "drinks.fxml");
+        mainContainer.loadPopUpScreen(PopUpScreens.SELECT_FOOD, fxmlRootPath + "food.fxml");
         mainContainer.loadPopUpScreen(PopUpScreens.SELECT_TOPPING, fxmlRootPath + "toppings.fxml");
         mainContainer.loadPopUpScreen(PopUpScreens.SELECT_SIDE, fxmlRootPath + "sides.fxml");
-        
+
         mainContainer.setScreen(Screens.MAIN); // set the main screen at the start
         // grouping the scene to root.
         Group root = new Group();
